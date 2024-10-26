842d26d37915fd5b71296896797d1ffe8b0a544d
Yusril Firza <38584535+yusrilfirza@users.noreply.github.com>
2024-10-26 20:15:10 +0700

    Adjust responsive for all page (#5)

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -12,35 +12,41 @@ export default function ProductDetail(props) {
             {
                 isFetching ? 'Loading' : (
                     <>
-                        <div id="product-detail-column">
-                            <div className="product-image" id="product-detail-image">
-                                <Image
-                                    src={data.image}
-                                    width={400}
-                                    height={400}
-                                    style={{
-                                        objectFit: "contain"
-                                    }}
-                                    alt={`product-${data.id}`}
-                                />
-                            </div>
-                            <Review data={data.reviews} />
-                        </div>
-                        <div id="product-detail-column">
-                            <h1>
-                                {data.title}
-                            </h1>
-                            <div className="product-detail-sub-title">
-                                <div className="product-price">
-                                    <h3 className="dollar-symbol">$</h3>
-                                    <h3>{data.price}</h3>
+                        <div id="product-detail-wrapper">
+                            <div id="product-detail-column">
+                                <div className="product-image" id="product-detail-image">
+                                    <Image
+                                        src={data.image}
+                                        width={400}
+                                        height={400}
+                                        style={{
+                                            objectFit: "contain"
+                                        }}
+                                        alt={`product-${data.id}`}
+                                    />
                                 </div>
-                                <Rate rate={data.rating.rate} />
+                                <div id="product-content">
+                                    <h1>
+                                        {data.title}
+                                    </h1>
+                                    <div className="product-detail-sub-title">
+                                        <div className="product-price">
+                                            <h3 className="dollar-symbol">$</h3>
+                                            <h3>{data.price}</h3>
+                                        </div>
+                                        <Rate rate={data.rating.rate} />
+                                    </div>
+                                    <p id="product-detail-description">
+                                        {data.description}
+                                    </p>
+                                    <ProductVariant data={variant} />
+                                </div>
                             </div>
-                            <p id="product-detail-description">
-                                {data.description}
-                            </p>
-                            <ProductVariant data={variant} />
+                            <div id="product-detail-column">
+                                <div id="product-review-wrapper">
+                                    <Review data={data.reviews} />
+                                </div>
+                            </div>
                         </div>
                     </>
                 )
